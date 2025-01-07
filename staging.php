0fb9ec8637d85cb9272d74b43b2c13a6f43f049b
Postali Web Development <webdev@postali.com>
2025-01-07 15:29:26 -0500

    updated for versioning

--- staging.php
+++ staging.php
@@ -73,6 +73,54 @@ function add_acf_field_function() {
                     'readonly' => 0,
                     'disabled' => 0,
                 ),
+
+                array (
+                    'key' => 'field_stagingUsername',
+                    'label' => 'Staging Username',
+                    'name' => 'staging_username',
+                    'type' => 'text',
+                    'prefix' => '',
+                    'instructions' => '',
+                    'required' => 0,
+                    'wrapper' => array (
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'prepend' => '',
+                    'append' => '',
+                    'maxlength' => '',
+                    'readonly' => 0,
+                    'disabled' => 0,
+                ),
+
+                array (
+                    'key' => 'field_stagingPassword',
+                    'label' => 'Staging Password',
+                    'name' => 'staging_password',
+                    'type' => 'text',
+                    'prefix' => '',
+                    'instructions' => '',
+                    'required' => 0,
+                    'wrapper' => array (
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'prepend' => '',
+                    'append' => '',
+                    'maxlength' => '',
+                    'readonly' => 0,
+                    'disabled' => 0,
+                ),
+
+
                 array (
                     'key' => 'field_slack_token',
                     'label' => 'Slack OAuth Token',
@@ -132,6 +180,8 @@ if (!function_exists('write_log')) {
 // Add Staging Lightbox
 function staging_admin_notice() {
     $staging_status = get_field('enable_banner','options');
+    $staging_username = get_field('staging_username','options');
+    $staging_password = get_field('staging_password','options');
     $site_name = get_bloginfo( 'name' ); 
     $currentUrl = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     $currentDomain = parse_url($currentUrl, PHP_URL_HOST);
@@ -152,6 +202,7 @@ function staging_admin_notice() {
                 </div>
                 <div id="notice-bottom">
                     <p>Proceed to the staging url before making changes: <a href="<?php echo $staging_url; ?>" title="Staging url"><?php echo $staging_url; ?></a></p>
+                    <p><strong>Staging Username:</strong> <?php echo $staging_username; ?> &nbsp; | &nbsp; <strong>Staging Password:</strong> <?php echo $staging_password; ?></p>
                     <p>Changes made here may be overwritten and lost. </br>Contact Development for more information.</p>
                 </div>
             </div>
@@ -247,4 +298,74 @@ function handle_staging_page_update( $post_id ) {
     }
 }
 add_action('acf/options_page/save', 'handle_staging_page_update', 10, 1);
+
+
+// Automatic plugin updates from the GitHub repository
+function automatic_GitHub_updates($data) {
+    // plugin information
+    //$plugin   = basename(get_template_directory()); // Folder name of the current plugin
+    $plugin = 'postali_staging';
+    $current = wp_get_plugin()->get('Version'); // Get the version of the current plugin
+    // GitHub information	    
+    $user = 'Postali-Webdev'; // The GitHub username hosting the repository
+    $repo = 'Postali_staging'; // Repository name as it appears in the URL
+    $token = get_field('github_token', 'option'); //https://github.com/settings/personal-access-tokens/new
+    $file = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/releases/latest', false,
+            stream_context_create(['http' => ['header' => "User-Agent: ".$user."\r\nAuthorization: token $token\r\n"]])
+        ));
+
+    if($file) {
+        $zip_source = $file->zipball_url;
+        $update = filter_var($file->tag_name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        // Only return a response if the new version number is higher than the current version
+        if($update > $current) {
+            $data->response[$plugin] = array(
+                'plugin'       =>  'Postali_staging-main',
+                // Strip the version number of any non-alpha characters (excluding the period)
+                // This way you can still use tags like v1.1 or ver1.1 if desired
+                'new_version' => $update,
+                'url'         => 'https://github.com/'.$user.'/'.$repo,
+                'package'     => $zip_source
+            );
+        }
+    }
+    return $data;
+
+    echo $data;    
+}
+add_filter('pre_set_site_transient_update_plugins', 'automatic_GitHub_updates', 100, 1);
+
+
+
+function rename_updated_plugin($true, $hook_extra, $result) {
+    // Check if the updated plugin is the one we want to rename
+    if ($hook_extra['plugin'] === 'Postali_staging-main') {
+        // Get the path to the updated plugin
+        $plugin_dir = $result['destination'];
+        // Rename the plugin directory
+        $new_plugin_dir = WP_CONTENT_DIR . '/plugins/Postali_staging-main';
+        if (rename($plugin_dir, $new_plugin_dir)) {
+            // Rename successful
+        } else {
+            // Rename failed, log an error
+            error_log("Failed to rename plugin directory to $new_plugin_dir");
+        }   
+    }
+}
+add_action('upgrader_post_install', 'rename_updated_plugin', 10, 3);
+
+
+
+add_filter('http_request_args', function($parsed_args, $url) {
+    $token = get_field('github_token', 'option'); //https://github.com/settings/personal-access-tokens/new
+    $user = 'Postali-Webdev'; // The GitHub username hosting the repository
+    $repo = 'Postali_staging'; // Repository name as it appears in the URL
+
+    if (str_contains($url, "$user/$repo")) {
+        $parsed_args['headers'] = ['Authorization'=> 'token '.$token];
+    }
+    return $parsed_args;
+}, 10, 2);
+
+
 ?>
